cda73bc6061db3c6e64145908dadcfc442afae5b
thanh lang viet <langvietthanh.28072005@gmail.com>
2026-05-10 06:22:37 +0000

    update api

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,10 @@ const AdminRouter = require("./AdminRouter");
 const authMiddleware = require("../middleware");
 
 const router = function (app) {
-    app.use("/admin", AdminRouter);
-    app.use("/user", UserRouter);
-    app.use("/photos", authMiddleware, PhotoRouter);
-    app.use("/", authMiddleware, CommentRouter);
-}
+  app.use("/admin", AdminRouter);
+  app.use("/api/user", UserRouter);
+  app.use("/api/photos", authMiddleware, PhotoRouter);
+  app.use("/api/comment", authMiddleware, CommentRouter);
+};
 
 module.exports = router;
